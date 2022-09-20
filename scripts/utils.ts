50f95e720b25836f999ba6e5ee4e53dd79e35b60
Matt Stam <mattstam@live.com>
2022-09-20 12:06:13 -0700

    add integration tests and deployment scripts for Token and Governor clone/factory (#5) 0x34f845773D4364999f2fbC7AA26ABDeE902cBb46

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,10 +1,21 @@
 import { expect } from "chai";
-import { BaseContract, ContractReceipt, ContractTransaction, utils } from "ethers";
-import { Interface } from "ethers/lib/utils";
+import {
+    BaseContract,
+    BigNumber,
+    BigNumberish,
+    ContractReceipt,
+    ContractTransaction,
+    utils,
+} from "ethers";
+import { concat, Interface } from "ethers/lib/utils";
+import { ethers } from "hardhat";
 
+import { hexlify } from "@ethersproject/bytes";
 import { TransactionReceipt } from "@ethersproject/providers";
 
 import { REPORT_GAS } from "../hardhat.config";
+import { PromiseOrValue } from "../types/common";
+import { ZERO_HASH } from "./const";
 
 // --- Transaction & contract deployment helpers ---
 
@@ -89,4 +100,71 @@ export function parseEvent(
         }
     }
     return res;
+}
+
+// --- EVM Time helpers ---
+
+export const period = {
+    seconds: function (val: number) {
+        return val;
+    },
+    minutes: function (val: number) {
+        return val * this.seconds(60);
+    },
+    hours: function (val: number) {
+        return val * this.minutes(60);
+    },
+    days: function (val: number) {
+        return val * this.hours(24);
+    },
+};
+
+// Wait for N number of blocks with a default 12 second interval in-between.
+// ~12 seconds is average after the PoW beacon change, but this still might need
+// to be adjusted for certain test cases.
+export async function waitBlocks(count: number, seconds: number = 12): Promise<void> {
+    const blocks = ethers.utils.hexValue(count).toString();
+    const interval = ethers.utils.hexValue(seconds);
+
+    await ethers.provider.send(`hardhat_mine`, [blocks, interval]);
+}
+
+// --- Math helpers ---
+
+// Choosing items randomly until all are taken and only then repeating
+// https://stackoverflow.com/a/17891411/15757416
+export function randomAvoidRepeats(array: any[]) {
+    let copy = array.slice(0);
+    return function () {
+        if (copy.length < 1) {
+            copy = array.slice(0);
+        }
+        const index = Math.floor(Math.random() * copy.length);
+        const item = copy[index];
+        copy.splice(index, 1);
+        return item;
+    };
+}
+
+export function randomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// OZ ERC20Votes has `type(uint224).max` check on mint, so must fit into 224 bits (28 bytes)
+// but for us this might also be used to generate MULTIPLE distribution values, so really
+// play it safe and default to 20.
+export function randomBigNumber(bytes = 20): BigNumber {
+    // zero-padding left
+    return ethers.BigNumber.from(
+        hexlify(concat([ethers.utils.randomBytes(bytes), ZERO_HASH]).slice(0, bytes)),
+    );
+}
+
+// calculate total sum from all individual values in the array
+export async function sumBigNumbers(values: PromiseOrValue<BigNumberish>[]): Promise<BigNumberish> {
+    let sum = ethers.BigNumber.from(0);
+    for (const val of values) {
+        sum = sum.add(await val);
+    }
+    return sum;
 }
